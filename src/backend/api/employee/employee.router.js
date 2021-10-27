142b37177d79ca56ca3b642cf8864a2f8d8b1816
Kim Chi <kimchilethi411@gmail.com>
2021-10-27 13:12:41 +0700

    create contract, edit employer, deploy heroku

--- src/backend/api/employee/employee.router.js
+++ src/backend/api/employee/employee.router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { EmployeeController } = require('./employee.controller');
 
 router.post('/', EmployeeController.createOne);
-router.get('/:id', EmployeeController.getOne);
+// router.get('/:id', EmployeeController.getOne);
 router.put('/:id', EmployeeController.updateOne);
 
 module.exports.EmployeeRouter = router;
