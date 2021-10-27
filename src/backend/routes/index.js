a9f14b13ee7d4625c8ac54146949dc2c125304ee
Kim Chi <kimchilethi411@gmail.com>
2021-10-27 13:09:38 +0700

    create contract, edit employer, deploy heroku

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const { ApiRouter } = require('../api/api.route');
 
-const renderView = view => (req, res) => res.render(view);
+// const renderView = view => (req, res) => res.render(view);
 
 router.use('/a/api', ApiRouter);
 
-router.get('/', renderView('signin'));
-router.get('/upload', renderView('upload'));
+// router.get('/', renderView('signin'));
+// router.get('/upload', renderView('upload'));
 
 module.exports = router;
